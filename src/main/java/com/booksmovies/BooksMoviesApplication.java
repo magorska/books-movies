83f08352d3ec81280780f6511af24499e0048fbf
magorska <magorska.95@gmail.com>
2021-09-13 15:38:28 +0200

    First commit

---
+++ src/main/java/com/booksmovies/BooksMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.booksmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksMoviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksMoviesApplication.class, args);
+    }
+
+}
